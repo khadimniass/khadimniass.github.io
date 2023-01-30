6a666e05c6545f359c8b6d1aec0d50a7ad49d175
khadim niass <niassssn@gmail.com>
2023-01-30 16:09:37 +0000

    ajouter le certificati et dahbrd openclassroom

--- projets/projet2/caroussel-last-version/script.js
+++ projets/projet2/caroussel-last-version/script.js
@@ -36,8 +36,23 @@ let Caroussel = function ([...image]) {
 	}
 }
 // setInterval(() => {
-let ImagesFoods = new Caroussel (["img/foods/food1.jpg","img/foods/food2.jpg", "img/foods/food3.jpg","img/foods/food4.PNG","img/foods/food5.jpg","img/foods/food5.jpg","img/foods/food6.jpg",
-	"img/foods/food7.jpg","img/foods/food8.jpg","img/foods/food9.jpg","img/foods/food10.jpg","img/foods/food12.jpg","img/foods/food13.PNG","img/foods/food14.PNG"]);
+let ImagesFoods = new Caroussel ([
+	"img/foods/food1.jpg",
+	"img/foods/food2.jpg",
+	"img/foods/food3.jpg",
+	"img/foods/food4.PNG",
+	"img/foods/food5.jpg",
+	"img/foods/food5.jpg",
+	"img/img1.jpg",
+	// "img/foods/food6.jpg",
+	// "img/foods/food7.jpg",
+	// "img/foods/food8.jpg",
+	// "img/foods/food9.jpg",
+	// "img/foods/food10.jpg",
+	// "img/foods/food12.jpg",
+	// "img/foods/food13.PNG",
+	// "img/foods/food14.PNG"
+]);
 	ImagesFoods.createurImage();
 	ImagesFoods.navigationLeftRight();
 // }, 1000);
